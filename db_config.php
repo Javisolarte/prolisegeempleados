2060e7b7bcd22cd4c83d9a9b2824e2ffca2d6b47
javisolarte <oscarjavisolarte@gmail.com>
2025-06-01 18:16:57 -0500

    Subiendo login con conexión a base de datos

--- db_config.php
+++ db_config.php
@@ -11,4 +11,22 @@ $conexion = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conexion) {
     die("Error al conectar con la base de datos: " . mysqli_connect_error());
 }
+?><?php
+// Mostrar errores (solo mientras desarrollas)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Configuración de la base de datos
+$servername = "162.241.61.240";
+$username = "empre479_Solarte";
+$password = "1234567891011121314151617181920";
+$dbname = "empre479_proliseg_db";
+
+// Crear la conexión a la base de datos
+$conexion = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if (!$conexion) {
+    die("Error al conectar con la base de datos: " . mysqli_connect_error());
+}
 ?>
