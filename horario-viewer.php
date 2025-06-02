44fd5df0da2906220da6c951d9be7c5e57ae5194
javisolarte <oscarjavisolarte@gmail.com>
2025-06-02 14:34:23 -0500

    Horarios2

--- horario-viewer.php
+++ horario-viewer.php
@@ -315,69 +315,69 @@
             </div>
         </div>
     </div>
+<!-- jQuery -->
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
 
-    <!-- jquery
-        ============================================ -->
-    <script src="js/vendor/jquery-1.12.4.min.js"></script>
-    <!-- bootstrap JS
-        ============================================ -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- wow JS
-        ============================================ -->
-    <script src="js/wow.min.js"></script>
-    <!-- price-slider JS
-        ============================================ -->
-    <script src="js/jquery-price-slider.js"></script>
-    <!-- meanmenu JS
-        ============================================ -->
-    <script src="js/jquery.meanmenu.js"></script>
-    <!-- owl.carousel JS
-        ============================================ -->
-    <script src="js/owl.carousel.min.js"></script>
-    <!-- sticky JS
-        ============================================ -->
-    <script src="js/jquery.sticky.js"></script>
-    <!-- scrollUp JS
-        ============================================ -->
-    <script src="js/jquery.scrollUp.min.js"></script>
-    <!-- counterup JS
-        ============================================ -->
-    <script src="js/counterup/jquery.counterup.min.js"></script>
-    <script src="js/counterup/waypoints.min.js"></script>
-    <script src="js/counterup/counterup-active.js"></script>
-    <!-- mCustomScrollbar JS
-        ============================================ -->
-    <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="js/scrollbar/mCustomScrollbar-active.js"></script>
-    <!-- metisMenu JS
-        ============================================ -->
-    <script src="js/metisMenu/metisMenu.min.js"></script>
-    <script src="js/metisMenu/metisMenu-active.js"></script>
-    <!-- morrisjs JS
-        ============================================ -->
-    <script src="js/morrisjs/raphael-min.js"></script>
-    <script src="js/morrisjs/morris.js"></script>
-    <script src="js/morrisjs/morris-active.js"></script>
-    <!-- morrisjs JS
-        ============================================ -->
-    <script src="js/sparkline/jquery.sparkline.min.js"></script>
-    <script src="js/sparkline/jquery.charts-sparkline.js"></script>
-    <script src="js/sparkline/sparkline-active.js"></script>
-    <!-- calendar JS
-        ============================================ -->
-    <script src="js/calendar/moment.min.js"></script>
-    <script src="js/calendar/fullcalendar.min.js"></script>
-    <script src="js/calendar/fullcalendar-active.js"></script>
-    <!-- plugins JS
-        ============================================ -->
-    <script src="js/plugins.js"></script>
-    <!-- main JS
-        ============================================ -->
-    <script src="js/main.js"></script>
-    <!-- tawk chat JS
-        ============================================ -->
-    <script src="js/pdf/jquery.media.js"></script>
-    <script src="js/pdf/pdf-active.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- WOW.js -->
+<script src="js/wow.min.js"></script>
+
+<!-- Price Slider -->
+<script src="js/jquery-price-slider.js"></script>
+
+<!-- MeanMenu -->
+<script src="js/jquery.meanmenu.js"></script>
+
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+
+<!-- Sticky -->
+<script src="js/jquery.sticky.js"></script>
+
+<!-- ScrollUp -->
+<script src="js/jquery.scrollUp.min.js"></script>
+
+<!-- CounterUp -->
+<script src="js/counterup/jquery.counterup.min.js"></script>
+<script src="js/counterup/waypoints.min.js"></script>
+<script src="js/counterup/counterup-active.js"></script>
+
+<!-- Custom Scrollbar -->
+<script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/scrollbar/mCustomScrollbar-active.js"></script>
+
+<!-- MetisMenu -->
+<script src="js/metisMenu/metisMenu.min.js"></script>
+<script src="js/metisMenu/metisMenu-active.js"></script>
+
+<!-- MorrisJS (Gráficos) -->
+<script src="js/morrisjs/raphael-min.js"></script>
+<script src="js/morrisjs/morris.js"></script>
+<script src="js/morrisjs/morris-active.js"></script>
+
+<!-- Sparkline (Gráficos pequeños) -->
+<script src="js/sparkline/jquery.sparkline.min.js"></script>
+<script src="js/sparkline/jquery.charts-sparkline.js"></script>
+<script src="js/sparkline/sparkline-active.js"></script>
+
+<!-- Calendario -->
+<script src="js/calendar/moment.min.js"></script>
+<script src="js/calendar/fullcalendar.min.js"></script>
+<script src="js/calendar/fullcalendar-active.js"></script>
+
+<!-- PDF Viewer -->
+<script src="js/pdf/jquery.media.js"></script>
+<script src="js/pdf/pdf-active.js"></script>
+
+<!-- Tabs -->
+<script src="js/tab.js"></script>
+
+<!-- Plugins y Main -->
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
 
 </body>
 
