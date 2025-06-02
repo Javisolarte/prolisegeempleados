ca8c988dfedc0bd5c7d6908499103142f7f2210d
javisolarte <oscarjavisolarte@gmail.com>
2025-06-02 02:12:24 -0500

    admin-user

--- todos-empleados-user.php
+++ todos-empleados-user.php
@@ -149,7 +149,7 @@
                                                 <li class="nav-item">
                                                         <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
                                                             <img src="img/product/1.png" alt="" />
-                                                            <span class="admin-name">Administrador</span>
+                                                            <span class="admin-name">Usuario</span>
                                                             <i class="fa fa-angle-down edu-icon edu-down-arrow"></i>
                                                         </a>
                                                         <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
